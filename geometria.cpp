7d0f39760f43b477066cca186cd9a90242c31a2b
luis-erf <luisedu2395@gmail.com>
2025-12-11 14:56:25 -0300

    Refactor point structure and add operator overloads

--- geometria.cpp
+++ geometria.cpp
@@ -1,58 +1,43 @@
 //PONTO
-// struct point_i { int x, y; }; // basic raw form, minimalist mode
-struct point_i { int x, y; // whenever possible, work with point_i
-point_i() { x = y = 0; } // default constructor
-point_i(int _x, int _y) : x(_x), y(_y) {} }; // user-defined
-struct point { double x, y; // only used if more precision is needed
-point() { x = y = 0.0; } // default constructor
-point(double _x, double _y) : x(_x), y(_y) {} };
-
-
-struct point { double x, y;
-point() { x = y = 0.0; }
-point(double _x, double _y) : x(_x), y(_y) {}
-bool operator < (point other) const { // override less than operator
-if (fabs(x - other.x) > EPS) // useful for sorting
-return x < other.x; // first criteria , by x-coordinate
-return y < other.y; } }; // second criteria, by y-coordinate
-// in int main(), assuming we already have a populated vector<point> P
-sort(P.begin(), P.end()); // comparison operator is defined above
-
-
-struct point { double x, y;
-point() { x = y = 0.0; }
-point(double _x, double _y) : x(_x), y(_y) {}
-// use EPS (1e-9) when testing equality of two floating points
-bool operator == (point other) const {
-return (fabs(x - other.x) < EPS && (fabs(y - other.y) < EPS)); } };
-// in int main()
-point P1(0, 0), P2(0, 0), P3(0, 1);
-printf("%d\n", P1 == P2); // true
-printf("%d\n", P1 == P3);
+struct point { 
+  double x, y;
+  point() { x = y = 0.0; }
+  point(double _x, double _y) : x(_x), y(_y) {}
+  bool operator < (point other) const { // override less than operator
+  if (fabs(x - other.x) > 0) // useful for sorting
+  return x < other.x; // first criteria , by x-coordinate
+  return y < other.y; } 
+  // use EPS (1e-9) when testing equality of two floating points
+  bool operator == (point other) const {
+  return (fabs(x - other.x) < EPS && (fabs(y - other.y) < EPS)); };
+};
 
 double dist(point p1, point p2) { // Euclidean distance
-// hypot(dx, dy) returns sqrt(dx * dx + dy * dy)
-return hypot(p1.x - p2.x, p1.y - p2.y); } // return double
+  // hypot(dx, dy) returns sqrt(dx * dx + dy * dy)
+  return hypot(p1.x - p2.x, p1.y - p2.y); 
+} // return double
 
 
 // rotate p by theta degrees CCW w.r.t origin (0, 0)
 point rotate(point p, double theta) {
-double rad = DEG_to_RAD(theta); // multiply theta with PI / 180.0
-return point(p.x * cos(rad) - p.y * sin(rad),
-p.x * sin(rad) + p.y * cos(rad)); }
+  double rad = DEG_to_RAD(theta); // multiply theta with PI / 180.0
+  return point(p.x * cos(rad) - p.y * sin(rad),
+  p.x * sin(rad) + p.y * cos(rad)); 
+}
 
 //LINHA
 struct line { double a, b, c; };
 
 // the answer is stored in the third parameter (pass by reference)
 void pointsToLine(point p1, point p2, line &l) {
-if (fabs(p1.x - p2.x) < EPS) { // vertical line is fine
-l.a = 1.0; l.b = 0.0; l.c = -p1.x; // default values
-} else {
-l.a = -(double)(p1.y - p2.y) / (p1.x - p2.x);
-l.b = 1.0; // IMPORTANT: we fix the value of b to 1.0
-l.c = -(double)(l.a * p1.x) - p1.y;
-} }
+  if (fabs(p1.x - p2.x) < EPS) { // vertical line is fine
+  l.a = 1.0; l.b = 0.0; l.c = -p1.x; // default values
+  } else {
+  l.a = -(double)(p1.y - p2.y) / (p1.x - p2.x);
+  l.b = 1.0; // IMPORTANT: we fix the value of b to 1.0
+  l.c = -(double)(l.a * p1.x) - p1.y;
+  } 
+}
 
 bool areParallel(line l1, line l2) { // check coefficients a & b
 return (fabs(l1.a-l2.a) < EPS) && (fabs(l1.b-l2.b) < EPS); }
@@ -61,22 +46,48 @@ return areParallel(l1 ,l2) && (fabs(l1.c - l2.c) < EPS); }
 
 // returns true (+ intersection point) if two lines are intersect
 bool areIntersect(line l1, line l2, point &p) {
-if (areParallel(l1, l2)) return false; // no intersection
-// solve system of 2 linear algebraic equations with 2 unknowns
-p.x = (l2.b * l1.c - l1.b * l2.c) / (l2.a * l1.b - l1.a * l2.b);
-// special case: test for vertical line to avoid division by zero
-if (fabs(l1.b) > EPS) p.y = -(l1.a * p.x + l1.c);
-else p.y = -(l2.a * p.x + l2.c);
-return true; }
+  if (areParallel(l1, l2)) return false; // no intersection
+  // solve system of 2 linear algebraic equations with 2 unknowns
+  p.x = (l2.b * l1.c - l1.b * l2.c) / (l2.a * l1.b - l1.a * l2.b);
+  // special case: test for vertical line to avoid division by zero
+  if (fabs(l1.b) > EPS) p.y = -(l1.a * p.x + l1.c);
+  else p.y = -(l2.a * p.x + l2.c);
+  return true; 
+}
 
-struct vec { double x, y; // name: ‘vec’ is different from STL vector
-vec(double _x, double _y) : x(_x), y(_y) {} };
+struct vec { 
+  double x, y; // name: ‘vec’ is different from STL vector
+  vec(double _x, double _y) : x(_x), y(_y) {} 
+};
 vec toVec(point a, point b) { // convert 2 points to vector a->b
-return vec(b.x - a.x, b.y - a.y); }
+    return vec(b.x - a.x, b.y - a.y); 
+}
 vec scale(vec v, double s) { // nonnegative s = [<1 .. 1 .. >1]
-return vec(v.x * s, v.y * s); } // shorter.same.longer
+  return vec(v.x * s, v.y * s); 
+} // shorter.same.longer
 point translate(point p, vec v) { // translate p according to v
-return point(p.x + v.x , p.y + v.y); }
+  return point(p.x + v.x , p.y + v.y); 
+}
+
+//MAIN: first, find P0 = point with lowest Y and if tie: rightmost X
+int P0 = 0;
+for (i = 1; i < n; i++)
+if (P[i].y < P[P0].y || (P[i].y == P[P0].y && P[i].x > P[P0].x))
+P0 = i;
+point temp = P[0]; P[0] = P[P0]; P[P0] = temp; // swap P[P0] with P[0]
+// second, sort points by angle w.r.t. pivot P0
+pivot = P[0]; // use this global variable as reference
+sort(++P.begin(), P.end(), angleCmp); // we do not sort P[0]
+// continuation from the earlier part
+// third, the ccw tests
+vector<point> S;
+S.push_back(P[n-1]); S.push_back(P[0]); S.push_back(P[1]); // initial S
+i = 2; // then, we check the rest
+while (i < n) { // note: N must be >= 3 for this method to work
+  j = (int)S.size()-1;
+  if (ccw(S[j-1], S[j], P[i])) S.push_back(P[i++]); // left turn, accept
+  else S.pop_back(); 
+} 
 
 
 double dot(vec a, vec b) { return (a.x * b.x + a.y * b.y); }
@@ -89,7 +100,8 @@ double distToLine(point p, point a, point b, point &c) {
 vec ap = toVec(a, p), ab = toVec(a, b);
 double u = dot(ap, ab) / norm_sq(ab);
 c = translate(a, scale(ab, u)); // translate a to c
-return dist(p, c); } // Euclidean distance between p and c
+return dist(p, c); 
+} // Euclidean distance between p and c
 
 // returns the distance from p to the line segment ab defined by
 // two points a and b (still OK if a == b)
@@ -101,11 +113,13 @@ if (u < 0.0) { c = point(a.x, a.y); // closer to a
 return dist(p, a); } // Euclidean distance between p and a
 if (u > 1.0) { c = point(b.x, b.y); // closer to b
 return dist(p, b); } // Euclidean distance between p and b
-return distToLine(p, a, b, c); } // run distToLine as above
+return distToLine(p, a, b, c); 
+} // run distToLine as above
 
 double angle(point a, point o, point b) { // returns angle aob in rad
 vec oa = toVector(o, a), ob = toVector(o, b);
-return acos(dot(oa, ob) / sqrt(norm_sq(oa) * norm_sq(ob))); }
+return acos(dot(oa, ob) / sqrt(norm_sq(oa) * norm_sq(ob))); 
+}
 
 double cross(vec a, vec b) { return a.x * b.y - a.y * b.x; }
 // note: to accept collinear points, we have to change the ‘> 0’
@@ -206,7 +220,7 @@ for (int i = 0; i < (int)P.size()-1; i++) {
 if (ccw(pt, P[i], P[i+1]))
 sum += angle(P[i], pt, P[i+1]); // left turn/ccw
 else sum -= angle(P[i], pt, P[i+1]); } // right turn/cw
-return fabs(fabs(sum) - 2*PI) < EPS; }
+return fabs(fabs(sum) - 2*PI) < 1e-9; }
 
 // line segment p-q intersect with line A-B.
 point lineIntersectSeg(point p, point q, point A, point B) {
@@ -233,32 +247,36 @@ return P; }
 
 point pivot(0, 0);
 bool angleCmp(point a, point b) { // angle-sorting function
-if (collinear(pivot, a, b)) // special case
-return dist(pivot, a) < dist(pivot, b); // check which one is closer
-double d1x = a.x - pivot.x, d1y = a.y - pivot.y;
-double d2x = b.x - pivot.x, d2y = b.y - pivot.y;
-return (atan2(d1y, d1x) - atan2(d2y, d2x)) < 0; } // compare two angles
+  if (collinear(pivot, a, b)) // special case
+  return dist(pivot, a) < dist(pivot, b); // check which one is closer
+  double d1x = a.x - pivot.x, d1y = a.y - pivot.y;
+  double d2x = b.x - pivot.x, d2y = b.y - pivot.y;
+  return (atan2(d1y, d1x) - atan2(d2y, d2x)) < 0;
+} // compare two angles
+
 vector<point> CH(vector<point> P) { // the content of P may be reshuffled
-int i, j, n = (int)P.size();
-if (n <= 3) {
-if (!(P[0] == P[n-1])) P.push_back(P[0]); // safeguard from corner case
-return P; } // special case, the CH is P itself
-// first, find P0 = point with lowest Y and if tie: rightmost X
-int P0 = 0;
-for (i = 1; i < n; i++)
-if (P[i].y < P[P0].y || (P[i].y == P[P0].y && P[i].x > P[P0].x))
-P0 = i;
-point temp = P[0]; P[0] = P[P0]; P[P0] = temp; // swap P[P0] with P[0]
-// second, sort points by angle w.r.t. pivot P0
-pivot = P[0]; // use this global variable as reference
-sort(++P.begin(), P.end(), angleCmp); // we do not sort P[0]
-// continuation from the earlier part
-// third, the ccw tests
-vector<point> S;
-S.push_back(P[n-1]); S.push_back(P[0]); S.push_back(P[1]); // initial S
-i = 2; // then, we check the rest
-while (i < n) { // note: N must be >= 3 for this method to work
-j = (int)S.size()-1;
-if (ccw(S[j-1], S[j], P[i])) S.push_back(P[i++]); // left turn, accept
-else S.pop_back(); } // or pop the top of S until we have a left turn
-return S; }
+  int i, j, n = (int)P.size();
+  if (n <= 3) {
+    if (!(P[0] == P[n-1])) P.push_back(P[0]); // safeguard from corner case
+    return P;
+  } // special case, the CH is P itself
+  // first, find P0 = point with lowest Y and if tie: rightmost X
+  int P0 = 0;
+  for (i = 1; i < n; i++)
+  if (P[i].y < P[P0].y || (P[i].y == P[P0].y && P[i].x > P[P0].x))
+  P0 = i;
+  point temp = P[0]; P[0] = P[P0]; P[P0] = temp; // swap P[P0] with P[0]
+  // second, sort points by angle w.r.t. pivot P0
+  pivot = P[0]; // use this global variable as reference
+  sort(++P.begin(), P.end(), angleCmp); // we do not sort P[0]
+  // continuation from the earlier part
+  // third, the ccw tests
+  vector<point> S;
+  S.push_back(P[n-1]); S.push_back(P[0]); S.push_back(P[1]); // initial S
+  i = 2; // then, we check the rest
+  while (i < n) { // note: N must be >= 3 for this method to work
+  j = (int)S.size()-1;
+  if (ccw(S[j-1], S[j], P[i])) S.push_back(P[i++]); // left turn, accept
+  else S.pop_back(); } // or pop the top of S until we have a left turn
+  return S; 
+}
